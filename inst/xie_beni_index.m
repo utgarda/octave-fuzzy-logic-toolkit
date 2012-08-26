cb45bea97de6375a564e1d3eb38d021e52816500
lmarkov <lmarkov@localhost>
2012-08-26 14:54:12 +0000

    M    inst/fcm.m M    inst/xie_beni_index.m M    inst/gustafson_kessel.m D    inst/fcm_demo_1.m M    inst/partition_entropy.m D    inst/fcm_demo_2.m D    inst/gustafson_kessel_demo_1.m D    inst/gustafson_kessel_demo_2.m M    inst/partition_coeff.m

--- inst/xie_beni_index.m
+++ inst/xie_beni_index.m
@@ -41,7 +41,7 @@
 ## and @var{soft_partition} matrices, please see the documentation for function
 ## fcm.
 ##
-## @seealso{fcm, fcm_demo_1, fcm_demo_2, gustafson_kessel, gustafson_kessel_demo_1, gustafson_kessel_demo_2, partition_coeff, partition_entropy}
+## @seealso{fcm, gustafson_kessel, partition_coeff, partition_entropy}
 ##
 ## @end deftypefn
 
@@ -49,7 +49,7 @@
 ## Keywords:      fuzzy-logic-toolkit fuzzy xie beni cluster validity
 ## Directory:     fuzzy-logic-toolkit/inst/
 ## Filename:      xie_beni_index.m
-## Last-Modified: 19 Aug 2012
+## Last-Modified: 26 Aug 2012
 
 function vxb = xie_beni_index (input_data, cluster_centers, ...
                                soft_partition)
@@ -126,3 +126,77 @@ function d_sqr_min = min_sqr_dist_between_centers (V)
   d_sqr_min = min (min (d_sqr_matrix));
 
 endfunction
+
+##----------------------------------------------------------------------
+## Xie-Beni Index Demo #1
+##----------------------------------------------------------------------
+
+%!demo
+%! ## Use the Fuzzy C-Means and Gustafson-Kessel algorithms to classify
+%! ## a small set of unlabeled data points and evaluate the quality
+%! ## of the resulting clusters.
+%!
+%! ## Note: The input_data is taken from Chapter 13, Example 17 in
+%! ##       Fuzzy Logic: Intelligence, Control and Information, by
+%! ##       J. Yen and R. Langari, Prentice Hall, 1999, page 381
+%! ##       (International Edition). 
+%! 
+%! input_data = [2 12; 4 9; 7 13; 11 5; 12 7; 14 4]
+%! number_of_clusters = 2
+%! 
+%! ## Using fcm, classify the input data, print the cluster centers,
+%! ## and calculate and print the partition coefficient.
+%! [cluster_centers, soft_partition, obj_fcn_history] = ...
+%!   fcm (input_data, number_of_clusters, [NaN NaN NaN 0]);
+%! puts ("\nResults using the Fuzzy C-Means algorithm:\n\n");
+%! cluster_centers
+%! printf ("Xie-Beni index: %f\n", ...
+%!        xie_beni_index (input_data, cluster_centers, soft_partition));
+%! 
+%! ## Using gustafson_kessel, classify the input data, print the cluster
+%! ## centers, and calculate and print the partition coefficient.
+%! [cluster_centers, soft_partition, obj_fcn_history] = ...
+%!   gustafson_kessel (input_data, number_of_clusters, [1 1 1], ...
+%!                     [NaN NaN NaN 0]);
+%! puts ("\nResults using the Gustafson-Kessel algorithm:\n\n");
+%! cluster_centers
+%! printf ("Xie-Beni index: %f\n\n", ...
+%!        xie_beni_index (input_data, cluster_centers, soft_partition));
+
+##----------------------------------------------------------------------
+## Xie-Beni Index Demo #2
+##----------------------------------------------------------------------
+
+%!demo
+%! ## Use the Fuzzy C-Means and Gustafson-Kessel algorithms to classify
+%! ## three-dimensional unlabeled data points and evaluate the quality
+%! ## of the resulting clusters.
+%!
+%! ## Note: The input_data was selected to form three areas of
+%! ##       different shapes.
+%!
+%! input_data = [1 11 5; 1 12 6; 1 13 5; 2 11 7; 2 12 6; 2 13 7; 3 11 6;
+%!               3 12 5; 3 13 7;  1 1 10; 1 3 9; 2 2 11; 3 1 9; 3 3 10;
+%!               3 5 11; 4 4 9; 4 6 8; 5 5 8; 5 7 9; 6 6 10; 9 10 12;
+%!               9 12 13; 9 13 14; 10 9 13; 10 13 12; 11 10 14;
+%!               11 12 13; 12 6 12; 12 7 15; 12 9 15; 14 6 14; 14 8 13]
+%! number_of_clusters = 3
+%!
+%! ## Using fcm, classify the input data, print the cluster centers,
+%! ## and calculate and print the partition coefficient.
+%! [cluster_centers, soft_partition, obj_fcn_history] = ...
+%!   fcm (input_data, number_of_clusters, [NaN NaN NaN 0]);
+%! puts ("\nResults using the Fuzzy C-Means algorithm:\n\n");
+%! cluster_centers
+%! printf ("Xie-Beni index: %f\n", ...
+%!        xie_beni_index (input_data, cluster_centers, soft_partition));
+%! 
+%! ## Using gustafson_kessel, classify the input data, print the cluster
+%! ## centers, and calculate and print the partition coefficient.
+%! [cluster_centers, soft_partition, obj_fcn_history] = ...
+%!   gustafson_kessel (input_data, number_of_clusters, [1 1 1], ...
+%!                     [NaN NaN NaN 0]);
+%! puts ("\nResults using the Gustafson-Kessel algorithm:\n\n");
+%! cluster_centers
+%! printf ("Xie-Beni index: %f\n\n", ...
+%!        xie_beni_index (input_data, cluster_centers, soft_partition));
